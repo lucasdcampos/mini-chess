f46d8485ebf3ad2b1f545fa4e54f776b2007ad1a
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-05-25 12:28:47 -0300

    Add Board Representation

---
+++ src/Board.cs
@@ -0,0 +1,61 @@
+public class Board
+{
+    public int[]? Squares { get; private set; }
+    public const string InitialPositionFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    public Board()
+    {
+        Squares = new int[64];
+        LoadPositionFromFen(InitialPositionFEN);
+    }
+
+    public void LoadPositionFromFen(string fen)
+    {
+        string piecePlacement = fen.Split(' ')[0];
+
+        int x = 0;
+        int y = 7;
+
+        foreach (char c in piecePlacement)
+        {
+            if (c == '/')
+            {
+                x = 0;
+                y--;
+            }
+            else if (char.IsDigit(c))
+            {
+                int emptySquares = c - '0';
+                for (int i = 0; i < emptySquares; i++)
+                {
+                    Squares![y * 8 + x] = Piece.None;
+                    x++;
+                }
+            }
+            else
+            {
+                int piece = Piece.None;
+                switch (c)
+                {
+                    case 'p': piece = Piece.Pawn | Piece.Black; break;
+                    case 'r': piece = Piece.Rook | Piece.Black; break;
+                    case 'n': piece = Piece.Knight | Piece.Black; break;
+                    case 'b': piece = Piece.Bishop | Piece.Black; break;
+                    case 'q': piece = Piece.Queen | Piece.Black; break;
+                    case 'k': piece = Piece.King | Piece.Black; break;
+                    case 'P': piece = Piece.Pawn | Piece.White; break;
+                    case 'R': piece = Piece.Rook | Piece.White; break;
+                    case 'N': piece = Piece.Knight | Piece.White; break;
+                    case 'B': piece = Piece.Bishop | Piece.White; break;
+                    case 'Q': piece = Piece.Queen | Piece.White; break;
+                    case 'K': piece = Piece.King | Piece.White; break;
+                    default:
+                        throw new ArgumentException($"Invalid character in FEN: '{c}'");
+                }
+
+                Squares![y * 8 + x] = piece;
+                x++;
+            }
+        }
+    }
+
+}
---
+++ src/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var board = new Board();
+        DrawBoard(board);
+    }
+
+    static void DrawBoard(Board board)
+    {
+        for (int y = 7; y >= 0; y--)
+        {
+            Console.Write("  +---+---+---+---+---+---+---+---+\n");
+            Console.Write($"{y + 1} ");
+
+            for (int x = 0; x < 8; x++)
+            {
+                int index = y * 8 + x;
+                int piece = board.Squares![index];
+
+                char symbol = GetPieceSymbol(piece);
+                Console.Write($"| {symbol} ");
+            }
+
+            Console.WriteLine("|");
+        }
+
+        Console.Write("  +---+---+---+---+---+---+---+---+\n");
+        Console.Write("    a   b   c   d   e   f   g   h\n");
+    }
+
+
+    static char GetPieceSymbol(int piece)
+    {
+        return piece switch
+        {
+            Piece.Pawn | Piece.White => 'P',
+            Piece.Pawn | Piece.Black => 'p',
+            Piece.Rook | Piece.White => 'R',
+            Piece.Rook | Piece.Black => 'r',
+            Piece.Knight | Piece.White => 'N',
+            Piece.Knight | Piece.Black => 'n',
+            Piece.Bishop | Piece.White => 'B',
+            Piece.Bishop | Piece.Black => 'b',
+            Piece.Queen | Piece.White => 'Q',
+            Piece.Queen | Piece.Black => 'q',
+            Piece.King | Piece.White => 'K',
+            Piece.King | Piece.Black => 'k',
+            _ => ' '
+        };
+    }
+}
